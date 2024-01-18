f779367f4bd1073104f87d4fa4f20847f796fd89
rds00013 <rds00013@mix.wvu.edu>
2024-01-18 16:36:51 -0500

    Episode 2 Completed

---
+++ src/main/java/net/ross/tutorialmod/item/ModItems.java
@@ -0,0 +1,25 @@
+package net.ross.tutorialmod.item;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.RegistryObject;
+import net.ross.tutorialmod.TutorialMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+
+}
